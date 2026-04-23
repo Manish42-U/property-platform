71e19facc5da10b8025667c4333cadb20832acc4
manish <manishkumar626772@gmail.com>
2026-04-23 17:11:54 +0530

    Add missing api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,26 +1,16 @@
-// src/services/api.js
 import axios from 'axios';
 
-// Production URL (Vercel env variable se lega)
 const BASE_URL = process.env.REACT_APP_API_URL || 'https://property-platform-4xcj.onrender.com/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  headers: { 'Content-Type': 'application/json' },
 });
 
-// Request interceptor to add token
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers['x-auth-token'] = token;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) config.headers['x-auth-token'] = token;
+  return config;
+});
 
 export default api;
